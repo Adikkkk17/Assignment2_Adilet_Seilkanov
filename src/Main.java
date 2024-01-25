c6bc8974470de8843ceacb156c4f94755c542ce6
adiletseilkanov <231590@astanait.edu.kz>
2024-01-25 23:13:55 +0600

    final commit

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,24 @@
+import java.util.ArrayList;
+import java.util.Collections;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-
-            System.out.println("i = " + i);
+        //Create list of people
+        ArrayList<Person> people = new ArrayList<>();
+        people.add(new Employee("Askar", "Khaimuldin", "Teacher", 30000.78));
+        people.add(new Employee("Nursultan", "Khaimuldin", "Lecturer", 31000.00));
+        people.add(new Student("Adilet", "Seilkanov", 2.5));
+        people.add(new Student("Umnik", "Umnikov", 3.8));
+        //sort list
+        Collections.sort(people);
+        //Print people
+        printData(people);
+    }
+    public static void printData(Iterable<Person> persons){
+        for(Person peron : persons){
+            System.out.println(peron.toString());
         }
     }
 }
