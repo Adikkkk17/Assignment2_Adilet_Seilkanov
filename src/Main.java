5089dbf396f503069e5509c400ee10626197ef78
adiletseilkanov <231590@astanait.edu.kz>
2024-04-09 21:51:47 +0500

    final commit

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,22 @@
-import java.util.ArrayList;
-import java.util.Collections;
+import java.util.Scanner;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        //Create list of people
-        ArrayList<Person> people = new ArrayList<>();
-        people.add(new Employee("Askar", "Khaimuldin", "Teacher", 30000.78));
-        people.add(new Employee("Nursultan", "Khaimuldin", "Lecturer", 31000.00));
-        people.add(new Student("Adilet", "Seilkanov", 2.5));
-        people.add(new Student("Umnik", "Umnikov", 3.8));
-        //sort list
-        Collections.sort(people);
-        //Print people
-        printData(people);
-    }
-    public static void printData(Iterable<Person> persons){
-        for(Person peron : persons){
-            System.out.println(peron.toString());
+    public static void BubbleSort(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length-1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
         }
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i]+" ");
+        }
+    }
+    public static void main(String[] args) {
+        int[] arr = {3,8,9,6,7};
+        BubbleSort(arr);
     }
 }
