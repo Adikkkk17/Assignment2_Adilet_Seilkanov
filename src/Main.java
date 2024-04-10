5be4518fa1a1045759e869f58940ac2e8e9ca53f
adiletseilkanov <231590@astanait.edu.kz>
2024-04-10 10:45:35 +0500

    final commit

--- src/Main.java
+++ src/Main.java
@@ -1,32 +1,17 @@
-import java.util.Scanner;
-
 public class Main {
-    public static void BubbleSort(int[] arr) {
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = 0; j < arr.length-1; j++) {
-                if (arr[j] > arr[j + 1]) {
-                    int temp = arr[j];
-                    arr[j] = arr[j + 1];
-                    arr[j + 1] = temp;
-                }
-            }
+
+    public static int sumOfNElements(int[] arr, int n) {
+        if (n <= 0) {
+            return 0;
         }
-        for (int i = 0; i < arr.length; i++) {
-            System.out.print(arr[i]+" ");
+        else {
+            return arr[n-1] + sumOfNElements(arr, n-1);
         }
     }
+
     public static void main(String[] args) {
-        int[] arr = {3,8,9,6,7};
-        BubbleSort(arr);
-
-
-
-
-
-
-
-
-
-
+        int[] arr = {1, 2, 3, 4, 5};
+        int n = 3;
+        System.out.println(sumOfNElements(arr, n));
     }
 }
